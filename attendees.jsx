6041f77567f1b848698c99e3ca5004c9783fa10e
oussking <43920179+oussking@users.noreply.github.com>
2018-10-09 10:34:29 +0100

    Create attendees.jsx

---
+++ attendees.jsx
@@ -0,0 +1,10 @@
+<div className="container mt-4">
+  <div className="row justify-content-center">
+    <div className="col-md-8">
+      <h1 className="font-weight-light text-center">
+        Attendees
+      </h1>
+    </div>
+  </div>
+  List Goes Here
+</div>
